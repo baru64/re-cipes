a448cb1b428eee1879ea2353a4cdb0b69fa61687
fpiwowarczyk <filip.piwowarczyk@nobl9.com>
2022-11-19 20:36:10 +0100

    Add grid MVP

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 import ProductList from "./ProductList.svelte";
+import RecipeGrid from "./RecipeGrid.svelte";
 </script>
 
 <main>
+    <RecipeGrid/>
     <ProductList></ProductList>
 </main>
