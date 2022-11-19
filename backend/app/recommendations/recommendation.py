3d3958ca2bd489176d6077593baaee1e27123ead
szymonbcoding <szymonbalawajder@gmail.com>
2022-11-19 23:57:31 +0100

    Added function get_eco_recommendation

--- backend/app/recommendations/recommendation.py
+++ backend/app/recommendations/recommendation.py
@@ -4,28 +4,11 @@ from surprise import Dataset
 from surprise import Reader
 from surprise import KNNWithMeans
 
-# column_names = ['user_id', 'item_id', 'rating', 'timestamp']
-# df = pd.read_csv('data/u.data', sep='\t', names=column_names)
-# # print(df.head())
-# # print(f"Shape df:{df.shape[0]}")
-
-# # print(np.unique(df['user_id'].to_numpy()))
-
-# df2 = df.loc[df['user_id']<10]
-# # print(f"Shape df2:{df2.shape[0]}")
-
-# df3 = df2.loc[df2['item_id']<100]
-# # print(f"Shape df3:{df3.shape[0]}")
-# # print(df3)
-# df3.to_csv(index=None, sep='\t', path_or_buf='data/ratings10users100items.csv')
-
-column_names = ['user_id', 'item_id', 'rating', 'timestamp']
-
-#functions
-
-# statistics
 
 def get_recommender_model(df: pd.DataFrame) -> KNNWithMeans:
+    """
+    Returns recommender based on user ratings.
+    """
     
     reader = Reader(rating_scale=(1, 5))
     data = Dataset.load_from_df(df[["user_id", "item_id", "rating"]], reader)
@@ -62,24 +45,35 @@ def get_n_best_rated_items_for_user(df: pd.DataFrame, n: int, user_id: int, mode
             # print(prediction.est)
             dftemp = pd.DataFrame([[user_id,x,prediction, 1]], columns = column_names)
             df_specific_user_id = pd.concat([df_specific_user_id, dftemp])
-            # df_specific_user_id.append(dftemp, ignore_index=True)
             
     return df_specific_user_id
-    
-    
-    
-    # prediction = model.predict(user_id,33)
-    # return df.groupby('item_id')['rating'].mean().sort_values(ascending=False)[:n]
 
-
+def get_eco_recommendation(my_cart: list, recipes):
+    """
+    Gets my_cart e.g. ['chicken', 'dried oregano']
+    and returns Datframe with column recipe_name and points
+    example row: Chicken Vesuvio 2
+    """
+    recipes_points = []
+    for x in recipes:
+        points = 0
+        for y in x['ingredients']:
+            if(y['name'] in my_cart):
+                points += 1
+        recipes_points.append([x['name'], points])
+    result = [x for x in recipes_points if x[1] > 0]
+    df = pd.DataFrame(result, columns = ['recipe_name', 'points'])
+    return df.sort_values('points', ascending=False)
 
 if __name__ == '__main__':
-    df = pd.read_csv('data/ratings10users100items.csv', sep='\t')
+    # df = pd.read_csv('data/ratings10users100items.csv', sep='\t')
 
-    model = get_recommender_model(df)
-
-    # prediction = model.predict(1,33)
-    # print(prediction.est)
-
-    df_with_recomms = get_n_best_rated_items_for_user(df, 5, 1, model)
+    # model = get_recommender_model(df)
+    
+    # df_with_recomms = get_n_best_rated_items_for_user(df, 5, 1, model)
     # df_with_recomms.to_csv(index=None, sep='\t', path_or_buf='data/ratingswithrecomms_userid1.csv')
+    
+    df = pd.read_csv('data/ratingswithrecomms_userid1.csv', sep='\t')
+    # df2 = df.loc[df['item_id']!=0]
+    
+    
