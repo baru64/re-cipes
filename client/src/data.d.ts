9f4ed1cb34788e905c7a86168d8d0196ca3ab359
Damian Jóźwiak <damian.jozwiak@nordcloud.com>
2022-11-19 15:56:44 +0100

    Added frontend data types

---
+++ client/src/data.d.ts
@@ -0,0 +1,40 @@
+// export interface Ingredient {
+//     name: String;
+//     text: String;
+//     quantity: Number;
+//     measure: Units;
+//     food: String;
+//     foodId?: String;
+//     image?: String;
+// }
+export interface Ingredient {
+    name: String;
+    text?: String;
+    measure: Units;
+    image?: String;
+}
+
+// export interface Product {
+//     food: String;
+//     name: String;
+//     quantity: Number;
+//     measure: Units;
+//     image: String;
+// }
+export interface Product extends Ingredient {
+    quantity: Number
+}
+
+export interface Recipe {
+    name: String;
+    description: String;
+    ingredients: Ingredient[];
+    picture: String;
+    ecoScore?: Number;
+}
+
+export enum Units {
+    G = 'g',
+    ML = 'ml',
+    PIECE = 'piece'
+}
